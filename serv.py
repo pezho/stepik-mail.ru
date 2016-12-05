2728346adf2d7857bfb5f9abbf4b2fc9823b9830
pezho <pezho.i@mail.ru>
2016-12-05 21:00:08 +0200

    v4

--- serv.py
+++ serv.py
@@ -2,34 +2,16 @@
 
 import socket
 
-class SocketManager:
 
-    def __init__(self, addres):
-        self.addres = addres
 
-    def __enter__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(self.addres)
-        self.sock.listen(1)
-        return self.sock
-
-    def __exit__(self, *ignore):
-        self.sock.close()
-
-def process(sock):
-    while True:
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('0.0.0.0', 2222))
+sock.listen(1)
+while True:
     conn, addr = sock.accept()
     while True:
         data = conn.recv(1024)
-        print(data)
-        if not data or 'close' in data.encode('utf-8'):
-            return
-
-        conn.sendall(data)
-
-
-
-with SocketManager(('0.0.0.0', 2222)) as sock:
-    # sock.listen(1)
-    process(sock)
+        if not data or 'close' in data.encode('utf9').lower():
+            break
+        conn.send(data)
 
